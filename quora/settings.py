ad8d463269307018af0cb7f28ea1f68ab8ac4d00
RajeswarSharma <54684919+RajeswarSharma@users.noreply.github.com>
2021-06-22 07:25:29 +0530

    Update settings.py

--- quora/settings.py
+++ quora/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+ADMINS = (
+  ('rajeswar', 'rjqwer47@gmail.com'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+SERVER_EMAIL = 'rjqwer47@gmail.com'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TAGGIT_CASE_INSENSITIVE = True
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
