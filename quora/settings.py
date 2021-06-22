eccf59413f7410a638407952f09fb38c31a6209c
RajeswarSharma <54684919+RajeswarSharma@users.noreply.github.com>
2021-06-22 06:30:34 +0530

    Update settings.py

--- quora/settings.py
+++ quora/settings.py
@@ -132,5 +132,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TAGGIT_CASE_INSENSITIVE = True
-#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 django_heroku.settings(locals())
